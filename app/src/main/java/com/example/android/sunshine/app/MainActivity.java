03819fbc930836da46f6dd517795864d07a3956b
Binit Kumar <kumar.binit1992@gmail.com>
2016-12-04 13:43:39 +0530

    Adding Maps into the menu, It will open location based on postalcode in SharedPreferences

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -44,6 +47,23 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this.getApplicationContext(),SettingsActivity.class));
 
             return true;
+        }else if(id == R.id.action_openinmap){
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+            String postalCode= pref.getString(getString(R.string.prefLocationKey),getString(R.string.prefLocationDefault));
+
+            // Create a Uri from an intent string. Use the result to create an Intent.
+           // Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
+            Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                    .appendQueryParameter("q", postalCode)
+                    .build();
+
+            // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, geoLocation);
+            // Make the Intent explicit by setting the Google Maps package
+            mapIntent.setPackage("com.google.android.apps.maps");
+
+            // Attempt to start an activity that can handle the Intent
+            startActivity(mapIntent);
         }
 
         return super.onOptionsItemSelected(item);
