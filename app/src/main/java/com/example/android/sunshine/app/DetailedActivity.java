b08c0f62d3d444997431b806ae683e6424634783
Kumar <inbkumar01@ad.harman.com>
2016-12-04 11:48:48 +0530

    Add Setting for location and Temperature units

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailedActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.sunshine.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class DetailedActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+    }
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -0,0 +1,7 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by inbkumar01 on 12/4/2016.
+ */
+public class SettingsActivity {
+}
